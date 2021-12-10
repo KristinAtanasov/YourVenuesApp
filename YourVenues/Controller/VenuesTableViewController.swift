75dfaa1fd4859df0ba96846c22c185a6bca7a233
Chriss Chris <zenskech@outlook.com>
2021-12-10 19:41:21 +0500

    adding comments for ui objects

--- YourVenues/Controller/VenuesTableViewController.swift
+++ YourVenues/Controller/VenuesTableViewController.swift
@@ -4,35 +4,47 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class VenuesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class VenuesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, MKMapViewDelegate, CLLocationManagerDelegate{
     
     let mapView = MKMapView()
     let tableView = UITableView()
     
+    let locationManager = CLLocationManager()
+    var location = CLLocation()
+    
     private var cellIdentifier = "cell"
     
-
+    
     var matchingItems = [MKMapItem]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
         searchVenues()
+        //showCurrentLocation()
+        checkLocationServices()
+        
+        
         // Set the table view data source and delegate to the current controller.
         tableView.dataSource = self
         tableView.delegate = self
         
         let screenSize = UIScreen.main.bounds
-        // Set the size for MapView and TableView and add it to the controller main view.
         
+        // Set the size for MapView and TableView and add it to the controller main view.
         mapView.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: (screenSize.height / 3))
         tableView.frame = CGRect(x: 0, y: mapView.frame.height, width: screenSize.width, height: screenSize.height)
+        
         view.addSubview(mapView)
         view.addSubview(tableView)
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
+        if CLLocationManager.locationServicesEnabled() {
+            // continue to implement here
+        } else {
+            // Do something to let users know why they need to turn it on.
+        }
         
     }
     
@@ -49,37 +61,69 @@ class VenuesTableViewController: UIViewController, UITableViewDataSource, UITabl
             guard let response = response else {
                 return
             }
+            
             self.matchingItems = response.mapItems
             self.tableView.reloadData()
         }
     }
     
+    
+    //        func showCurrentLocation(){
+    //                let coordinate = mapView.userLocation.coordinate
+    //                let center = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    //            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    //                self.mapView.setRegion(region, animated: true)
+    //            }
+    //
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            checkLocationAuthorization()
+        } else {
+            // Show alert letting the user know they have to turn this on.
+        }
+    }
+    func checkLocationAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+        case .denied: // Show alert telling users how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            mapView.showsUserLocation = true
+        case .restricted: // Show an alert letting them know what’s up
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
+        // MARK: - Table view data source
+        
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return matchingItems.count - 20
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+            
+            let venuesNames = matchingItems[indexPath.row]
+            
+            cell.textLabel?.text = venuesNames.name
+            
+            return cell
+            
+            
+        }
+    }
 
-    
-    // MARK: - Table view data source
-    
-   
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return matchingItems.count - 20
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        
-        let venuesNames = matchingItems[indexPath.row]
-        
-        cell.textLabel?.text = venuesNames.name
 
-        return cell
-        
-        
-    }
-}
+
 
 
 
